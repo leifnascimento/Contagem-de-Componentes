3a53096468694f8ea82859f8091dd4d0ad51c1f7
leifnascimento <leifbrasil@gmail.com>
2022-11-11 15:44:37 -0400

    Contagem de componentes

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("Montagem de Componentes:");
+  }, []);
+  useEffect(() => {
+    console.log("Contagem de Atualização:");
+  }, [count]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Count: {count}</Text>
+      <Button title="Incremento contagem" onPress={() => setCount(count + 1)} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +24,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
